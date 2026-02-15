1e3e45520e9d8e4b8fc9106f49c9c99c22cc2488
weaponsforge <forgeweapons@gmail.com>
2026-02-15 10:42:31 +0800

    feat: dockerize the app
    *feat: setup docker for localhost dev
    *feat: setup docker for production (next app router) standalone build
    *feat: setup static export for docker

--- nextapp/next.config.ts
+++ nextapp/next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from 'next'
 
+const IS_BUILD_STATIC = process.env.IS_BUILD_STATIC === '1'
+const IS_BUILD_DOCKER = process.env.IS_BUILD_DOCKER === '1'
+
+if (IS_BUILD_STATIC && IS_BUILD_DOCKER) {
+  throw new Error(
+    'Only 1 of IS_BUILD_STATIC or IS_BUILD_DOCKER could exist at a time',
+  )
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  ...(IS_BUILD_STATIC && {
+    // static export
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true, // Required for static export
+    },
+  }),
+  ...(IS_BUILD_DOCKER && {
+    // standalone build for docker production
+    output: 'standalone',
+  }),
 }
 
 export default nextConfig
