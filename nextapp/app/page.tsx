723013bad3f6d42e0994d43d41bbf02df238aa1a
weaponsforge <forgeweapons@gmail.com>
2026-02-15 10:24:22 +0800

    feat: use custom eslint rules and lint app

--- nextapp/app/page.tsx
+++ nextapp/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { Menu } from "./features";
+import Image from 'next/image'
+import { Menu } from '@/features'
 
 export default function Home() {
   return (
@@ -21,20 +21,20 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a starting point or more instructions? Head over to{' '}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-            </a>{" "}
-            or the{" "}
+            </a>{' '}
+            or the{' '}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
-            </a>{" "}
+            </a>{' '}
             center.
           </p>
         </div>
@@ -65,5 +65,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  );
+  )
 }
--- nextapp/eslint.config.mjs
+++ nextapp/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +8,32 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+  {
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-console': ['error', { 'allow': ['error'] }],
+      'react/react-in-jsx-scope': 'off',
+      'no-restricted-imports': [
+        'error',
+        { 'patterns':
+          [
+            '@/features/*',
+            './features/*'
+          ]
+        }
+      ],
+    },
+  },
+])
 
-export default eslintConfig;
+export default eslintConfig
